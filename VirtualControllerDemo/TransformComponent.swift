0afbd8a4718fe8b5949c5874c75120a34140d581
Swift Dev Journal <mark@swiftdevjournal.com>
2024-07-15 19:47:56 -0400

    Add the code from the Catcher game.

--- VirtualControllerDemo/TransformComponent.swift
+++ VirtualControllerDemo/TransformComponent.swift
@@ -6,3 +6,51 @@
 //
 
 import Foundation
+import SpriteKit
+import GameplayKit
+
+class TransformComponent: GKComponent {
+    private var node = SKNode()
+    
+    var position: CGPoint {
+        get {
+            node.position
+        }
+        set {
+            node.position = newValue
+        }
+    }
+    
+    var rotation:  CGFloat {
+        get {
+            node.zRotation
+        }
+        set {
+            node.zRotation = newValue
+        }
+    }
+    
+    var scale: CGSize {
+        get {
+            return .init(width: node.xScale, height: node.yScale)
+        }
+        set {
+            node.xScale = newValue.width
+            node.yScale = newValue.height
+        }
+    }
+
+    func set(node: SKNode) {
+        node.position = position
+        node.zRotation = rotation
+        node.xScale = scale.width
+        node.yScale = scale.height
+        
+        self.node = node
+    }
+    
+    func translate(_ direction: CGVector) {
+        node.position.x += direction.dx
+        node.position.y += direction.dy
+    }
+}
