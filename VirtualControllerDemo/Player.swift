157b48f995d9a5e199f89e59f8a3c099f23bde95
Swift Dev Journal <mark@swiftdevjournal.com>
2024-07-15 19:49:54 -0400

    Fill in the initial shell of the Player class from the Catcher game demo.

--- VirtualControllerDemo/Player.swift
+++ VirtualControllerDemo/Player.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+import GameplayKit
+
+class Player: GKEntity {
+    let sprite = SpriteComponent()
+    let transform = TransformComponent()
+    var score = 0
+    
+    override init() {
+        super.init()
+        
+        sprite.node = SKSpriteNode(color: .red, size: CGSize(width: 128, height: 128))
+        addComponent(sprite)
+        
+        addComponent(transform)
+        transform.set(node: sprite.node)        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
