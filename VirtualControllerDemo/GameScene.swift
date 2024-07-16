57d6c85147705dcc1030a189819f466603f30d75
Swift Dev Journal <mark@swiftdevjournal.com>
2024-07-16 13:59:52 -0400

    Remove the references to the entities, graphs, and last update time that were included when creating the project. This should simplify the code and make it easier to read.

--- VirtualControllerDemo/GameScene.swift
+++ VirtualControllerDemo/GameScene.swift
@@ -11,14 +11,9 @@ import GameController
 
 class GameScene: SKScene {
     var player = Player()
-    var entities = [GKEntity]()
-    var graphs = [String : GKGraph]()
-    
-    private var lastUpdateTime : TimeInterval = 0
     
     override func sceneDidLoad() {
         placePlayer()
-        self.lastUpdateTime = 0
     }
     
     func placePlayer() {
@@ -30,7 +25,6 @@ class GameScene: SKScene {
         addChild(player.sprite.node)
     }
     
-        
     func touchDown(atPoint pos : CGPoint) {
 
     }
@@ -51,37 +45,25 @@ class GameScene: SKScene {
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         for t in touches {
-            self.touchMoved(toPoint: t.location(in: self)) }
+            self.touchMoved(toPoint: t.location(in: self))
+        }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         for t in touches {
-            self.touchUp(atPoint: t.location(in: self)) }
+            self.touchUp(atPoint: t.location(in: self))
+        }
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
         for t in touches {
-            self.touchUp(atPoint: t.location(in: self)) }
+            self.touchUp(atPoint: t.location(in: self))
+        }
     }
     
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
-
-        // Initialize _lastUpdateTime if it has not already been
-        if (self.lastUpdateTime == 0) {
-            self.lastUpdateTime = currentTime
-        }
-        
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
-        
-        // Update entities
-        for entity in self.entities {
-            entity.update(deltaTime: dt)
-        }
-        
-        self.lastUpdateTime = currentTime
     }
     
 }
